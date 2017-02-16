10750dbc4330b13dc087498672791d6dfc7b38aa
Jinsong <jinsonghan1996@gmail.com>
2017-02-16 16:59:46 -0500

    Added listener that appends CORS headers

---
+++ src/AppBundle/Listener/CORSListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Database;
+use AppBundle\Controller\AuthenticationRequiredController;
+use AppBundle\Controller\AdminRequiredController;
+use AppBundle\Controller\ErrorController;
+
+/**
+ * This listener adds CORS headers to every response.
+ */
+class CORSListener
+{
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+    }
+
+
+}
