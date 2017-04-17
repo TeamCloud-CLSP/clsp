5a25c4e016b9a6b002094d44b2cfa328c99a3a49
Jinsong <jhan320@gatech.edu>
2017-04-17 15:16:12 -0400

    added more test data to database

--- clsp.sql
+++ clsp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 17, 2017 at 04:44 AM
+-- Generation Time: Apr 17, 2017 at 07:15 PM
 -- Server version: 5.7.17-0ubuntu0.16.04.1
 -- PHP Version: 7.0.8-2+deb.sury.org~xenial+1
 
@@ -107,7 +107,12 @@ INSERT INTO `module_cn` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (6, 6, '', NULL, 0, 0, 0),
 (7, 10, '', NULL, 0, 1, 0),
 (8, 11, '', NULL, 0, 1, 0),
-(9, 12, '', NULL, 0, 1, 0);
+(9, 12, '', NULL, 0, 1, 0),
+(10, 18, '', NULL, 0, 0, 0),
+(11, 19, '', NULL, 0, 0, 0),
+(12, 20, '', NULL, 0, 0, 0),
+(13, 21, '', NULL, 0, 0, 0),
+(14, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_cn_keyword`
@@ -150,7 +155,12 @@ INSERT INTO `module_dw` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (6, 6, '', NULL, 0, 0, 0),
 (7, 10, '', NULL, 0, 1, 0),
 (8, 11, '', NULL, 0, 1, 0),
-(9, 12, '', NULL, 0, 0, 0);
+(9, 12, '', NULL, 0, 0, 0),
+(10, 18, '', NULL, 0, 0, 0),
+(11, 19, '', NULL, 0, 0, 0),
+(12, 20, '', NULL, 0, 0, 0),
+(13, 21, '', NULL, 0, 0, 0),
+(14, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_ge`
@@ -163,7 +173,12 @@ INSERT INTO `module_ge` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (6, 6, '', NULL, 0, 0, 0),
 (7, 10, '', NULL, 0, 1, 0),
 (8, 11, '', NULL, 0, 1, 0),
-(9, 12, '', NULL, 0, 1, 0);
+(9, 12, '', NULL, 0, 1, 0),
+(10, 18, '', NULL, 0, 0, 0),
+(11, 19, '', NULL, 0, 0, 0),
+(12, 20, '', NULL, 0, 0, 0),
+(13, 21, '', NULL, 0, 0, 0),
+(14, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_ls`
@@ -176,7 +191,12 @@ INSERT INTO `module_ls` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (6, 6, '', NULL, 0, 0, 0),
 (7, 10, '', NULL, 1, 0, 0),
 (8, 11, '', NULL, 0, 0, 0),
-(9, 12, '', NULL, 0, 0, 0);
+(9, 12, '', NULL, 0, 0, 0),
+(10, 18, '', NULL, 0, 0, 0),
+(11, 19, '', NULL, 0, 0, 0),
+(12, 20, '', NULL, 0, 0, 0),
+(13, 21, '', NULL, 0, 0, 0),
+(14, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_lt`
@@ -189,7 +209,12 @@ INSERT INTO `module_lt` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (6, 6, '', NULL, 0, 0, 0),
 (7, 10, '', NULL, 0, 1, 0),
 (8, 11, '', NULL, 0, 1, 0),
-(9, 12, '', NULL, 0, 1, 0);
+(9, 12, '', NULL, 0, 1, 0),
+(10, 18, '', NULL, 0, 0, 0),
+(11, 19, '', NULL, 0, 0, 0),
+(12, 20, '', NULL, 0, 0, 0),
+(13, 21, '', NULL, 0, 0, 0),
+(14, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_qu`
@@ -202,7 +227,12 @@ INSERT INTO `module_qu` (`id`, `song_id`, `password`, `name`, `has_password`, `i
 (8, 6, 'valleystone', NULL, 1, 1, 1),
 (9, 10, '', NULL, 0, 1, 0),
 (10, 11, '', NULL, 0, 0, 0),
-(11, 12, '', NULL, 0, 1, 0);
+(11, 12, '', NULL, 0, 1, 0),
+(12, 18, '', NULL, 0, 0, 0),
+(13, 19, '', NULL, 0, 0, 0),
+(14, 20, '', NULL, 0, 0, 0),
+(15, 21, '', NULL, 0, 0, 0),
+(16, 22, '', NULL, 0, 0, 0);
 
 --
 -- Dumping data for table `module_question_heading`
@@ -238,14 +268,14 @@ INSERT INTO `module_question_item` (`id`, `heading_id`, `content`, `type`, `weig
 (3, 3, 'test-not-meu', 'test', 3, NULL, NULL),
 (4, 2, 'test-meu-3, heading 2', 'multiple-choice', 1, NULL, ''),
 (5, 1, 'Ibuki Izumi of Hinabita plays the _, while Meu plays the _.', 'fill-blank', 2, '', '[{"choice":"bass"},{"choice":"drums:drum:percussion"}]'),
-(6, 11, 'CLOSER was released in year _.', 'fill-blank', 1, NULL, '[{"choice":"2009"}]'),
-(7, 11, 'What was the name of the original single that CLOSER was released on?', 'multiple-choice', 2, '[{"choice":"ME ME ME"},{"choice":"CLOSER"},{"choice":"Colors"},{"choice":"Silhouette"}]', '[{"choice":"CLOSER"}]'),
-(8, 11, 'What languages did Joe Inoue eventually cover or sing CLOSER in?', 'multiple-select', 3, '[{"choice":"Korean"},{"choice":"Japanese"},{"choice":"English"},{"choice":"Spanish"}]', '[{"choice":"Japanese"},{"choice":"English"}]'),
+(6, 11, '「CLOSER」は_年から発売されました。', 'fill-blank', 1, NULL, '[{"choice":"2008"}]'),
+(7, 11, '「CLOSER」と言う歌は最初にシングルに発表しました。このシングルのタイトルはなんですか。', 'multiple-choice', 2, '[{"choice":"GRAVITY"},{"choice":"CLOSER"},{"choice":"海色"},{"choice":"影踏み"}]', '[{"choice":"CLOSER"}]'),
+(8, 11, '「CLOSER」はどの言葉で歌いましたか。', 'multiple-select', 3, '[{"choice":"韓国語"},{"choice":"日本語"},{"choice":"英語"},{"choice":"スペイン語"}]', '[{"choice":"日本語"},{"choice":"英語"}]'),
 (9, 12, 'What does 偽善 mean?', 'multiple-choice', 1, '[{"choice":"To lose sight of"},{"choice":"Treasure"},{"choice":"Surreal"},{"choice":"Hypocrisy"}]', '[{"choice":"Hypocrisy"}]'),
 (10, 12, '勇気を胸に literally means to have _ in the _.', 'fill-blank', 2, '', '[{"choice":"bravery:strength:courage"},{"choice":"heart:chest"}]'),
-(11, 13, 'Why do you think the music video for the song features only Joe Inoue representing all characters?', 'short-answer', NULL, NULL, NULL),
-(12, 13, 'In your own terms (and to the best of your ability), describe what is happening in the music video.', 'short-answer', NULL, NULL, NULL),
-(13, 14, 'What do you think the title, CLOSER, was trying to convey?', 'short-answer', NULL, NULL, NULL);
+(11, 13, 'どうして「CLOSER」のミュージックビデオの人は全部井上ジョーですか。', 'short-answer', 1, NULL, NULL),
+(12, 13, 'このミュージックビデオはどんな物語を見せましたか。', 'short-answer', 2, NULL, NULL),
+(13, 14, 'この歌のタイトル、「CLOSER」、の意味はなんですか。', 'short-answer', 1, NULL, NULL);
 
 --
 -- Dumping data for table `professor_registrations`
@@ -267,9 +297,14 @@ INSERT INTO `song` (`id`, `unit_id`, `title`, `weight`, `album`, `artist`, `desc
 (2, 3, 'ちくわパフェだよ☆CKP', 2, 'Bitter Sweet Girls', '日向美ビタースイーツ♪', 'chikuwa parfait', 'ちくパちくパ  ちくわのパフェなんだよ！ ちくパちくパ  おいしいめう  おしゃれめう！ ちくパちくパ  CKPCKP  ちくパちくパ  ちくパ最高ーッ！  わぁー！  生クリーム  バニラのアイスに  イチゴとバナナと メインはもちろん（もちろん）  ちくわめう！（ちくわ！） とろけたチョコをかければ完成  ちくパで大丈夫だよ  絶対  大丈夫だよっ！  ｢んきゅーッ！？悪のスイーツ大魔王が現れためう！｣ ｢落ち着いて！このちくわステッキで魔法ちくわ少女に変身するんだよっ！｣ ｢そそそそんな設定聞いてないめう！｣ ｢大丈夫だよ！絶対、大丈夫だよっ！｣  ちくパちくパ  まりまり  ちくパちくパ  もりもり ちくパちくパ  めうめう  ちくパちくパ  めりめり ちくちわ  ちくちくわ  ちくちくちわ  ちくちくちくわ  ちくちくちくちわ  ｢めうーっ！ちくわが折れたぁ！｣ ｢大丈夫！必殺・ちくわの穴から生クリーム光線だよっ！｣ （ドグシャァ　バリバリドッカー　チチクワァ） ｢こうして町に平和が訪れためう｣ ｢ちくわのおかげだねっ！｣  ちくパの味は似ているね  パパパウーパールーパー（違うめう！） みんな笑顔  やぶれかぶれ  無敵のハーモニー ちくパの歯ごたえ  お手前  パパパグーパーチョキパー（勝負めう！） ちくわの穴のぞけば  ほら  キラキラ光る  未来が見える  おーっ！ ちくパちくパ  CKPCKP  ちくパちくパ  ちくパ最高ーッ！                     ｢せーの！｣                        ｢｢ちくパ！｣｣', NULL),
 (3, 4, 'NOT 4361 UNIT 3', 3, 'NOT 4361 UNIT 3', 'NOT 4361 UNIT 3', 'NOT 4361 UNIT 3', 'NOT 4361 UNIT 3', NULL),
 (6, 3, '君がいる場所へ', 3, 'Form Ride', 'VALLEYSTONE', 'The last song on VALLEYSTONE\'s first album, Form Ride. Also featured in SOUND VOLTEX III GRAVITY WARS.', '君だけのため　光る世界\r\nここにあるよ――\r\n\r\n幼い手で握りしめてた\r\n失くさないよう　ぎゅっと\r\n真っ白な　願いを\r\n君と追いかけて\r\n\r\nまた迷い込む　寂しげな夜だから\r\nあの日の面影を　見つけて\r\n思い出すよ\r\n\r\n君がいるから　どこでだって\r\n何度、転んでも　笑える\r\n偶然だとか　必然だとか\r\n要らないほど　強く\r\nきっと輝く　思い出なら\r\n君の側にだけ　あるから\r\n繋いだ手は　信じてるよ\r\n\r\nその温かさが　嬉しくて\r\nずっと守れるように\r\n強くなりたかった\r\nほら　君のように\r\n\r\nもっと沢山の歌詞は ※ Mojim.com\r\n一緒に歩き　重ねたこの季節は\r\nどれだけの思いも　願いも\r\n分かっているの\r\n\r\nもう大丈夫　君がいれば\r\n心、それだけで　強くなる\r\n過去と未来　どこでもない\r\n今ここにあるから\r\n届いてるかな　君の元へ\r\n明日　また逢える　奇跡を\r\n忘れないで\r\nいつまでも――\r\n\r\nあの日　君と会った　その引力が\r\nそっと　物語を　溶かしていくの\r\nちっぽけな　私も　小さな芽を\r\n優しく抱いて　誰かにいつか\r\n届けたいよ\r\n\r\n君がいるから　どこでだって\r\n何度、転んでも　笑える\r\n偶然だとか　必然だとか\r\n要らないほど　強く\r\nきっと輝く　思い出なら\r\n君の側にだけ　あるから\r\n大切な世界　離さないよ', NULL),
-(10, 9, 'CLOSER', 1, 'CLOSER', 'Joe Inoue', 'The title track of the second single of Joe Inoue (井上ジョー). Although released in 2009, it is one of his most popular works to date. The song was used in the Naruto television series.', '身近にある物 \r\n常に気を付けていないと \r\n余りに近すぎて \r\n見失ってしまいそう \r\n\r\nあなたが最近体験した \r\n幸せは一体何ですか? \r\n恵まれすぎていて \r\n思い出せないかも! \r\n\r\n今ここにいる事 \r\n息をしている事 \r\nただそれだけの事が \r\n奇跡だと気付く \r\n\r\n身近にある物 \r\n常に気を付けていないと \r\n余りに近すぎて \r\n見失ってしまいそう \r\n\r\nYou know the closer you get to something \r\nThe tougher it is to see it \r\nAnd I\'ll never take it for granted \r\n\r\nLet\'s go \r\n\r\n人助けを偽善と \r\n呼ぶ奴等もいるけれど \r\n信じるのも疑うのも \r\n人それぞれだから \r\n\r\nたとえ仮にそれが \r\n偽善であったとしても \r\n誰かを救えたなら \r\nそりゃむしろ何よりもリアル \r\n\r\n追い続けてきた夢 \r\nあきらめずに進めよなんて \r\nキレイ事を言えるほど \r\n何も出来ちゃいないけど \r\n\r\n一握りの勇気を胸に \r\n明日を生き抜くために \r\nAnd I\'ll never take it for granted \r\n\r\nLet\'s go', NULL),
-(11, 9, 'ヒトヒラのハナビラ', 2, 'ハイドランジアが咲いている', 'STEREOPONY', 'The first single released in 2008 by the Japanese rock group, STEREOPONY. ', 'ヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だったそんな事 思いたくはない \r\n\r\nテーブルの向こう 暗い顔してる \r\n切り出す コトバに 怯えてんだ \r\nいったいいつから僕ら こんなキモチに \r\n気づかないふり続けてたんだ? \r\n出逢った日のような あの透き通る風の中で \r\nやり直せるのなら もう一度 抱きしめたい \r\n\r\nヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だった そんな事 思いたくはない \r\n\r\n好きだった はずだった いつだって声が聞きたくなるほど \r\nそれなのに 手が届く先の君が 見えなくなりそうだ \r\n\r\n平気だってすぐ我慢してたのは \r\nきっと僕たちの悪いトコで \r\nいっつも一緒にいたいって 想ってたのに \r\nすれ違いは現実を変えた? \r\n出逢った日のような あの柔らかな笑顔だって \r\nすぐに取り戻せる 気がしてた 抱きしめたい \r\n\r\nヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だった そんな事 思いたくはない \r\n\r\n黙ったままの君の手のひら \r\nたどり着いたナミダがハジけた \r\nこんな僕たちの 時間を巻き戻して・・・ \r\n\r\nヒトヒラの ハナビラが 舞い落ちた 僕のとなりで今 \r\n間違った 恋だった なんて忘れられるはずはないんだ \r\n\r\n好きだった 好きだった いまだってすがりつきたくなるほど \r\nそれなのに 手が届く先の君が 見えなくなりそうだ', NULL),
-(12, 9, 'Snow Halation', 3, 'Snow Halation', 'μ’s', 'The second single produced by μ’s in 2010. Arguably the most popular song that lead to their overwhelming fame and popularity in Japan.', '不思議だね　いまの気持ち \r\n空から降ってきたみたい \r\n特別な季節の色が　ときめきを見せるよ \r\n\r\n初めて出会った時から \r\n予\\感に騒ぐ心のMelody \r\nとめられないとまらない　な・ぜ \r\n\r\n届けて \r\n切なさには名前をつけようか“Snow halation” \r\n想いが重なるまで待てずに \r\n悔しいけど好きって純情 \r\n微熱の中　ためらってもダメだね \r\n飛び込む勇気に賛成 まもなくStart!! \r\n\r\n音もなく　気配もなく \r\n静かに運命は変わる \r\nこれからの未来に胸の　鼓動が早くなる \r\n\r\n例えば困った時には \r\nすぐ駆けつけて抱きしめたくて \r\nどこにいてもどこでも　Fly high \r\n\r\n急いで \r\nいつの間にか大きくなりすぎた“True emotion” \r\n夢だけみてる様じゃつらいよ \r\n恋人は君って言いたい \r\n優しい目が　とまどってるイヤだよ \r\nこのまま一気に愛情 あずけてPlease!! \r\n\r\n届けて \r\n切なさには名前をつけようか“Snow halation” \r\n想いが重なるまで待てずに \r\n悔しいけど好きって純情 \r\n微熱の中　ためらってもダメだね \r\n飛び込む勇気に賛成 まもなくStart!!', NULL);
+(10, 9, 'CLOSER', 1, 'CLOSER', '井上ジョー', '「CLOSER」井上ジョーの曲です。「CLOSER」は最初に2008年12月17日にKi/oon Recordsから発売された。シングルの名前は 「CLOSER」です。そして、「CLOSER」はテレビ東京系アニメ『ナルト』の第4期オープニングテーマでした。', '身近にある物 \r\n常に気を付けていないと \r\n余りに近すぎて \r\n見失ってしまいそう \r\n\r\nあなたが最近体験した \r\n幸せは一体何ですか? \r\n恵まれすぎていて \r\n思い出せないかも! \r\n\r\n今ここにいる事 \r\n息をしている事 \r\nただそれだけの事が \r\n奇跡だと気付く \r\n\r\n身近にある物 \r\n常に気を付けていないと \r\n余りに近すぎて \r\n見失ってしまいそう \r\n\r\nYou know the closer you get to something \r\nThe tougher it is to see it \r\nAnd I\'ll never take it for granted \r\n\r\nLet\'s go \r\n\r\n人助けを偽善と \r\n呼ぶ奴等もいるけれど \r\n信じるのも疑うのも \r\n人それぞれだから \r\n\r\nたとえ仮にそれが \r\n偽善であったとしても \r\n誰かを救えたなら \r\nそりゃむしろ何よりもリアル \r\n\r\n追い続けてきた夢 \r\nあきらめずに進めよなんて \r\nキレイ事を言えるほど \r\n何も出来ちゃいないけど \r\n\r\n一握りの勇気を胸に \r\n明日を生き抜くために \r\nAnd I\'ll never take it for granted \r\n\r\nLet\'s go', NULL),
+(11, 9, 'ヒトヒラのハナビラ', 2, 'ハイドランジアが咲いている', 'STEREOPONY', '「ヒトヒラのハナビラ」は、日本のロックバンド・ステレオポニーのメジャーデビューシングルである。', 'ヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だったそんな事 思いたくはない \r\n\r\nテーブルの向こう 暗い顔してる \r\n切り出す コトバに 怯えてんだ \r\nいったいいつから僕ら こんなキモチに \r\n気づかないふり続けてたんだ? \r\n出逢った日のような あの透き通る風の中で \r\nやり直せるのなら もう一度 抱きしめたい \r\n\r\nヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だった そんな事 思いたくはない \r\n\r\n好きだった はずだった いつだって声が聞きたくなるほど \r\nそれなのに 手が届く先の君が 見えなくなりそうだ \r\n\r\n平気だってすぐ我慢してたのは \r\nきっと僕たちの悪いトコで \r\nいっつも一緒にいたいって 想ってたのに \r\nすれ違いは現実を変えた? \r\n出逢った日のような あの柔らかな笑顔だって \r\nすぐに取り戻せる 気がしてた 抱きしめたい \r\n\r\nヒトヒラの ハナビラが 揺れている 僕のとなりで今 \r\n間違った 恋だった そんな事 思いたくはない \r\n\r\n黙ったままの君の手のひら \r\nたどり着いたナミダがハジけた \r\nこんな僕たちの 時間を巻き戻して・・・ \r\n\r\nヒトヒラの ハナビラが 舞い落ちた 僕のとなりで今 \r\n間違った 恋だった なんて忘れられるはずはないんだ \r\n\r\n好きだった 好きだった いまだってすがりつきたくなるほど \r\nそれなのに 手が届く先の君が 見えなくなりそうだ', NULL),
+(12, 9, 'Snow Halation', 3, 'Snow Halation', 'μ’s', '「Snow halation」（スノー・ハレーション）は、μ\'sの楽曲。同グループ2枚目のシングルとして2010年12月22日にLantisから発売された。CDには楽曲とボイスドラマが、DVDにはアニメーションミュージッククリップが収録されている。通称は「スノハレ」。', '不思議だね　いまの気持ち \r\n空から降ってきたみたい \r\n特別な季節の色が　ときめきを見せるよ \r\n\r\n初めて出会った時から \r\n予\\感に騒ぐ心のMelody \r\nとめられないとまらない　な・ぜ \r\n\r\n届けて \r\n切なさには名前をつけようか“Snow halation” \r\n想いが重なるまで待てずに \r\n悔しいけど好きって純情 \r\n微熱の中　ためらってもダメだね \r\n飛び込む勇気に賛成 まもなくStart!! \r\n\r\n音もなく　気配もなく \r\n静かに運命は変わる \r\nこれからの未来に胸の　鼓動が早くなる \r\n\r\n例えば困った時には \r\nすぐ駆けつけて抱きしめたくて \r\nどこにいてもどこでも　Fly high \r\n\r\n急いで \r\nいつの間にか大きくなりすぎた“True emotion” \r\n夢だけみてる様じゃつらいよ \r\n恋人は君って言いたい \r\n優しい目が　とまどってるイヤだよ \r\nこのまま一気に愛情 あずけてPlease!! \r\n\r\n届けて \r\n切なさには名前をつけようか“Snow halation” \r\n想いが重なるまで待てずに \r\n悔しいけど好きって純情 \r\n微熱の中　ためらってもダメだね \r\n飛び込む勇気に賛成 まもなくStart!!', NULL),
+(18, 8, '故郷', 1, 'なし', '岡野貞一', '故郷（ふるさと）は、高野辰之作詞・岡野貞一作曲による文部省唱歌。', '兎追いし　彼の山\r\n小鮒釣りし　彼の川\r\n夢は今も　巡りて\r\n忘れ難き　故郷\r\n\r\n如何にいます　父母\r\n恙無しや　友がき\r\n雨に風に　つけても\r\n思い出づる　故郷\r\n\r\n志を　果たして\r\nいつの日にか　帰らん\r\n山は靑き　故郷\r\n水は淸き　故郷', 'https://www.youtube.com/watch?v=jnrr_b6xWmI'),
+(19, 8, '海行かば', 2, 'なし', '信時潔', '『海行かば』（うみゆかば）とは、日本の軍歌ないし国民歌の一である。', '海行かば\r\n水漬く屍\r\n山行かば\r\n草生す屍\r\n大君の辺にこそ死なめ\r\nかえりみはせじ', 'https://www.youtube.com/watch?v=yUwvIFYcCqo'),
+(20, 8, '露営の歌', 3, 'なし', '古関裕而', '露営の歌（ろえいのうた）は、1937年（昭和12年）9月にコロムビアレコードから発売された軍歌。', 'なし', NULL),
+(21, 10, '七人の侍', 1, '映画', '黒澤明', '『七人の侍』（しちにんのさむらい）は、1954年（昭和29年）4月26日に公開された日本映画である。東宝製作・配給。監督は黒澤明、主演は三船敏郎と志村喬。モノクロ、スタンダード・サイズ、207分。', '映画ですから、歌詞はありません。', NULL),
+(22, 10, 'となりのトトロ', 2, 'となりのトトロ', '中川李枝子/井上杏美', '卜トロ トトロ トトロ トトロ \r\nだれかが こっそり \r\n小路に 木の実 うずめて \r\nちっさな芽 生えたら 秘密の暗号 \r\n森へのパスポート \r\nすてきな冒険はじまる \r\nとなりのトトロ トトロ トトロ トトロ \r\n\r\n森の中に むかしから住んでる \r\nとなりのトトロ トトロ トトロ トトロ \r\n子供のときにだけ あなたに訪れる \r\n不思議な出会い \r\n\r\n雨ふり バス停 \r\nズブヌレ オバケがいたら \r\nあなたの雨ガサ さしてあげましょ \r\n森へのパスポート \r\n魔法の扉 あきます \r\nとなりのトトロ トトロ トトロ トトロ \r\n月夜の晩に オカリナ吹いてる \r\nとなりのトトロ トトロ トトロ トトロ \r\nもしも会えたなら すてきな しあわせが \r\nあなたに来るわ \r\n\r\n卜卜ロ 卜トロ 卜トロ 卜トロ \r\n森の中に むかしから住んでる \r\nとなりのトトロ トトロ トトロ トトロ \r\n子供のときにだけ あなたに訪れる \r\n不思議な出会い \r\n\r\nトトロ トトロ トトロ トトロ \r\nトロ トトロ 卜トロ トトロ……', '映画ですから、歌詞はありません。', NULL);
 
 --
 -- Dumping data for table `songs_media`
@@ -302,10 +337,10 @@ INSERT INTO `unit` (`id`, `course_id`, `name`, `description`, `weight`) VALUES
 (3, 3, 'Unit 2 - 4361', 'Historic Japan', 2),
 (4, 1, 'Unit 1 - CHINESE 3002', 'THIS DOES NOT BELONG TO 4361', 10),
 (6, 3, 'Unit 1 - 4361', 'Introduction to Syllabus', 1),
-(7, 4, 'Unit 1 - Introduction', 'Welcome to Japanese 4215: Modern Japanese Culture!', 1),
-(8, 4, 'Unit 2 - Brief History', 'A quick overview of culture and history up to the 21st century.', 2),
-(9, 4, 'Unit 3 - Popular Songs', 'Several popular songs released in the 21st century will be discussed in this unit, along with their impact on popular culture.', 3),
-(10, 4, 'Unit 4 - Popular Movies', 'Several popular movies and their impact on Japanese culture will be discussed in this unit.', 4);
+(7, 4, 'Unit 1 - 前書き\n', '日本語４２１５にようこそう。', 1),
+(8, 4, 'Unit 2 - 民謡\n', '民謡を使って、21世紀までの文化と歴史を少し勉強しましょう。', 2),
+(9, 4, 'Unit 3 - 現代の歌\n', '21世紀に作った人気な曲を見ましょう。これらの曲は世話にインパクトした。', 3),
+(10, 4, 'Unit 4 - 現代の映画\n', '21世紀に作った人気な映画をみて、映画のテーマとメッセージを話します。', 4);
 SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
